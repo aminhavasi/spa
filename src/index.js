5563446410747ca3df6a29eeb2516932cbf773a2
amin havasi <pau.ahq@gmail.com>
2020-03-06 12:48:06 +0330

    React router dom added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+import App from "./App";
+import NotFound from "./components/common/redirect";
 
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/not-found" component={NotFound} />
+            <Route exact path="/" component={App} />
+            <Redirect to="/not-found" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+);
