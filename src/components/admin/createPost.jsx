a97d918f852dd5349805f757005b247e375d39a7
amin havasi <pau.ahq@gmail.com>
2020-03-08 20:04:06 +0330

    create-post

---
+++ src/components/admin/createPost.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+class Cpost extends Component {
+    state = {
+        postTitle: '',
+        postImageUrl: '',
+        postContent: '',
+        postTags: []
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+    render() {
+        return (
+            <form
+                className="form-group bg-light border rounded m-2 shadow p-5"
+                onSubmit={this.handleSubmit}
+            >
+                <label className="col-md-4 control-label " htmlFor="txttitle">
+                    post Title:
+                </label>
+                <input
+                    id="txtTitle"
+                    name="postTitle"
+                    placeholder="title..."
+                    className="form-control input-md m-2"
+                    value={this.state.postTitle}
+                    onChange={e => this.setState({ postTitle: e.target.value })}
+                />
+                <input
+                    id="txtImageUrl"
+                    name="postImageUrl"
+                    placeholder="image link (Not required)"
+                    className="form-control input-md m-2"
+                    value={this.state.postImageUrl}
+                    onChange={e =>
+                        this.setState({ postImageUrl: e.target.value })
+                    }
+                />
+                <textarea
+                    name="postContent"
+                    className="form-control m-2"
+                    rows="5"
+                    placeholder="postContent"
+                    value={this.state.postContent}
+                    onChange={e =>
+                        this.setState({ postContent: e.target.value })
+                    }
+                />
+                <label className="col-md-4 control-label" htmlFor="txtTags">
+                    Tags:
+                </label>
+                <input
+                    id="txtTags"
+                    name="postTags"
+                    type="text"
+                    placeholder="tags - spread tags with (',') "
+                    className="form-control input-md m-2"
+                    value={this.state.postTags}
+                    onChange={e =>
+                        this.setState({
+                            postTags: _.split(e.target.value, ',')
+                        })
+                    }
+                />
+                <button className="btn btn-primary m-5">Create new post</button>
+            </form>
+        );
+    }
+}
+export default Cpost;
--- src/components/admin/dashborad.jsx
+++ src/components/admin/dashborad.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Navbar from './navbar';
 import Sidebar from './sidebar';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Cpost from './createPost';
 
 function Dashboard() {
     return (
-        <div className="App">
+        <div>
             <Navbar />
             <Sidebar />
+            <main
+                className="col-md-9 mr-sm-auto  col-lg-10 px-4"
+                style={{ marginLeft: '0', marginTop: '70px' }}
+            >
+                <Switch>
+                    <Route path="/admin/create-post" component={Cpost} />
+                </Switch>
+            </main>
         </div>
     );
 }
